f95ab624e252496db85f06c34b36faf0866b6147
captnoord <captnoord@53638f16-6364-11df-b3cf-17e690f86674>
2008-11-02 12:03:23 +0000

    * more love for the xmlpktgen project

--- xmlpktgen/DecodeGenerator.cpp
+++ xmlpktgen/DecodeGenerator.cpp
@@ -1,9 +1,32 @@
+/*
+	------------------------------------------------------------------------------------
+	LICENSE:
+	------------------------------------------------------------------------------------
+	This file is part of EVEmu: EVE Online Server Emulator
+	Copyright 2006 - 2008 The EVEmu Team
+	For the latest information visit http://evemu.mmoforge.org
+	------------------------------------------------------------------------------------
+	This program is free software; you can redistribute it and/or modify it under
+	the terms of the GNU Lesser General Public License as published by the Free Software
+	Foundation; either version 2 of the License, or (at your option) any later
+	version.
+
+	This program is distributed in the hope that it will be useful, but WITHOUT
+	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+	FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License along with
+	this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+	Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+	http://www.gnu.org/copyleft/lesser.txt.
+	------------------------------------------------------------------------------------
+*/
 
 #include "DecodeGenerator.h"
 #include "../common/logsys.h"
 
 #ifndef WIN32
-#warning Decoder is not properly freeing old items in the case of object re-use
+#  warning Decoder is not properly freeing old items in the case of object re-use
 #endif
 
 ClassDecodeGenerator::ClassDecodeGenerator()
@@ -1339,25 +1362,3 @@ bool ClassDecodeGenerator::Process_real(FILE *into, TiXmlElement *field) {
 	pop();
 	return(true);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
