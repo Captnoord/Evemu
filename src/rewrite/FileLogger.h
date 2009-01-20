2cec01e06c8f04da9a3455184d647297b0174899
captnoord <captnoord@53638f16-6364-11df-b3cf-17e690f86674>
2009-01-20 11:07:45 +0000

    * unix linkage fix.

--- src/rewrite/FileLogger.h
+++ src/rewrite/FileLogger.h
@@ -72,6 +72,17 @@ public:
 		logPacket(packet->content(), packet->size(), direction);
 	}
 
+	void logSimplePacket(PyTupleStream* packet, int direction)
+	{
+		if (mEnableFileWrite == false)
+			return;
+
+		if (packet == NULL)
+			return;
+
+		logSimplePacket(packet->content(), packet->size(), direction);
+	}
+
 #define ENDING_WINDOWS "\r\n"
 #define ENDING_UNIX "\n"
 // use unix line endings..
@@ -179,6 +190,109 @@ public:
 		fflush(fouttxt);
 	}
 
+	// simple packets are streams we supply the header for its very slow... hehe
+	void logSimplePacket(const unsigned char* xdata, size_t size, unsigned int direction)
+	{
+		unsigned char* data = new unsigned char[size + 5];
+		
+		memcpy(data+5, xdata, size);
+		data[0] = 0x7E;
+		data[1] = 0;
+		data[2] = 0;
+		data[3] = 0;
+		data[4] = 0;
+
+		unsigned int line = 1;
+		unsigned int countpos = 0;
+		size_t lenght = size + 5;
+
+		fprintf(fouttxt, "{%s} PacketSize = %u"LINE_END, (direction ? "SERVER" : "CLIENT"), lenght);
+		fprintf(fouttxt, "|------------------------------------------------|----------------|"LINE_END);
+		fprintf(fouttxt, "|00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F |0123456789ABCDEF|"LINE_END);
+		fprintf(fouttxt, "|------------------------------------------------|----------------|"LINE_END);
+
+		if(lenght > 0)
+		{
+			fprintf(fouttxt, "|");
+			for (unsigned int count = 0 ; count < lenght ; count++)
+			{
+				if (countpos == 16)
+				{
+					countpos = 0;
+
+					fprintf(fouttxt,"|");
+
+					for (unsigned int a = count-16; a < count;a++)
+					{
+						if ((data[a] < 32) || (data[a] > 126))
+							fprintf(fouttxt,".");
+						else
+							fprintf(fouttxt,"%c",data[a]);
+					}
+
+					fprintf(fouttxt,"|"LINE_END);
+
+					line++;
+					fprintf(fouttxt,"|");
+				}
+
+				fprintf(fouttxt,"%02X ",data[count]);
+
+				//FIX TO PARSE PACKETS WITH LENGHT < OR = TO 16 BYTES.
+				if (count+1 == lenght && lenght <= 16)
+				{
+					for (unsigned int b = countpos+1; b < 16;b++)
+						fprintf(fouttxt,"   ");
+
+					fprintf(fouttxt,"|");
+
+					for (unsigned int a = 0; a < lenght;a++)
+					{
+						if ((data[a] < 32) || (data[a] > 126))
+							fprintf(fouttxt,".");
+						else
+							fprintf(fouttxt,"%c",data[a]);
+					}
+
+					for (unsigned int c = count; c < 15;c++)
+						fprintf(fouttxt," ");
+
+					fprintf(fouttxt,"|"LINE_END);
+				}
+
+				//FIX TO PARSE THE LAST LINE OF THE PACKETS WHEN THE LENGHT IS > 16 AND ITS IN THE LAST LINE.
+				if (count+1 == lenght && lenght > 16)
+				{
+					for (unsigned int b = countpos+1; b < 16;b++)
+						fprintf(fouttxt,"   ");
+
+					fprintf(fouttxt,"|");
+
+					unsigned short print = 0;
+
+					for (unsigned int a = line * 16 - 16; a < lenght;a++)
+					{
+						if ((data[a] < 32) || (data[a] > 126))
+							fprintf(fouttxt,".");
+						else
+							fprintf(fouttxt,"%c",data[a]);
+
+						print++;
+					}
+
+					for (unsigned int c = print; c < 16;c++)
+						fprintf(fouttxt," ");
+
+					fprintf(fouttxt,"|"LINE_END);
+				}
+
+				countpos++;
+			}
+		}
+		fprintf(fouttxt, "-------------------------------------------------------------------"LINE_END""LINE_END);
+		fflush(fouttxt);
+	}
+
 protected:
 
 	FILE * fouttxt;
@@ -189,7 +303,7 @@ protected:
 #endif
 };
 
-#endif//FILE_LOGGER_H
-
 #define sFileLogger FileLogger::getSingleton()
 initialiseSingleton(FileLogger);
+
+#endif//FILE_LOGGER_H
