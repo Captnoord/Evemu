31d7626b0c4df82df3f2fab063c531421b62aa4c
captnoord <captnoord@53638f16-6364-11df-b3cf-17e690f86674>
2009-01-23 14:38:34 +0000

    * small corrections that soon will be obsolete part 2 lol.

--- src/rewrite/MarshalReferenceMap.h
+++ src/rewrite/MarshalReferenceMap.h
@@ -3,7 +3,7 @@
 	LICENSE:
 	------------------------------------------------------------------------------------
 	This file is part of EVEmu: EVE Online Server Emulator
-	Copyright 2006 - 2008 The EVEmu Team
+	Copyright 2006 - 2009 The EVEmu Team
 	For the latest information visit http://evemu.mmoforge.org
 	------------------------------------------------------------------------------------
 	This program is free software; you can redistribute it and/or modify it under
@@ -26,8 +26,7 @@
 #ifndef UNMARSHAL_REFERENCE_MAP_H
 #define UNMARSHAL_REFERENCE_MAP_H
 
-#include "common.h"
-#include "PyRep.h"
+#include "Common.h"
 
 /**
  * MarshalReferenceMap.h only implement the UnmarshalReferenceMap, the Marshal reference tech
