195440e7c6a44f538fe48e07fc9e9afcb8eac09d
bloody.rabbit <bloody.rabbit@53638f16-6364-11df-b3cf-17e690f86674>
2010-07-29 11:22:12 +0000

    * added ThreadMgr

---
+++ include/common/mt/ThreadMgr.h
@@ -0,0 +1,171 @@
+/*
+    ------------------------------------------------------------------------------------
+    LICENSE:
+    ------------------------------------------------------------------------------------
+    This file is part of EVEmu: EVE Online Server Emulator
+    Copyright 2006 - 2008 The EVEmu Team
+    For the latest information visit http://evemu.mmoforge.org
+    ------------------------------------------------------------------------------------
+    This program is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the Free Software
+    Foundation; either version 2 of the License, or (at your option) any later
+    version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License along with
+    this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+    Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+    http://www.gnu.org/copyleft/lesser.txt.
+    ------------------------------------------------------------------------------------
+    Author:     Bloody.Rabbit
+*/
+
+#ifndef __MT__THREAD_MGR_H__INCL__
+#define __MT__THREAD_MGR_H__INCL__
+
+#include "mt/Condition.h"
+#include "mt/Mutex.h"
+#include "mt/Worker.h"
+#include "utils/Singleton.h"
+
+/**
+ * @brief Thread manager, does thread pooling.
+ *
+ * @author Bloody.Rabbit
+ */
+class ThreadMgr
+: public Singleton< ThreadMgr >
+{
+public:
+    /**
+     * @brief A primary constructor.
+     *
+     * @param[in] limit Thread count limit.
+     */
+    ThreadMgr( size_t limit = 100 );
+    /*
+     * @brief A primary destructor.
+     */
+    ~ThreadMgr();
+
+    /// Obtains current thread count.
+    size_t threadCount() const;
+    /// Obtains current active thread count.
+    size_t activeThreadCount() const;
+    /// Obtains current inactive thread count.
+    size_t inactiveThreadCount() const;
+
+    /// Obtains current thread count limit.
+    size_t threadLimit() const;
+
+    /**
+     * @brief Adds a new target to the processing queue.
+     *
+     * @param[in] target The new target.
+     */
+    void Run( Thread::Target* target );
+
+    /**
+     * @brief Sets new thread count limit.
+     *
+     * @param[in] limit The new limit.
+     */
+    void SetThreadLimit( size_t limit );
+
+protected:
+    class Worker;
+
+    /// Typedef for target queue.
+    typedef std::queue< Thread::Target* > TargetQueue;
+    /// Typedef for working threads list.
+    typedef std::list< Worker* > WorkerList;
+
+    /**
+     * @brief Registers new Worker.
+     *
+     * @param[in] worker The new worker to be registered.
+     *
+     * @return The worker's iterator.
+     */
+    WorkerList::iterator AddWorker( Worker* worker );
+    /**
+     * @brief Unregisters a Worker.
+     *
+     * @param[in] itr The worker's iterator.
+     */
+    void RemoveWorker( WorkerList::iterator& itr );
+
+    /**
+     * @brief Obtains a new target.
+     *
+     * @param[in] itr The worker's iterator.
+     *
+     * @return A Target to process.
+     * @retval NULL A signal to quit.
+     */
+    Thread::Target* GetTarget( WorkerList::iterator& itr );
+
+    /// A queue of pending targets.
+    TargetQueue mTargets;
+    /// A list of active workers.
+    WorkerList mActiveWorkers;
+    /// A list of inactive workers.
+    WorkerList mInactiveWorkers;
+    /// A Condition for workers.
+    Condition mWorkerEvent;
+
+    /// Thread limit.
+    size_t mLimit;
+
+    /// A Mutex to protect this object.
+    mutable Mutex mMutex;
+};
+
+/// A macro for easier access.
+#define sThreadMgr \
+    ( ThreadMgr::get() )
+
+/**
+ * @brief A ThreadMgr's worker thread.
+ *
+ * This class assumes it's dynamically allocated,
+ * owned and run by only a single thread.
+ *
+ * @author Bloody.Rabbit
+ */
+class ThreadMgr::Worker
+: public ::Worker
+{
+public:
+    /// Primary constructor.
+    Worker( ThreadMgr* mgr );
+    /// A destructor.
+    ~Worker();
+
+    /**
+     * @brief Assigns a new manager.
+     *
+     * @param[in] mgr The new manager.
+     */
+    void AssignMgr( ThreadMgr* mgr );
+
+protected:
+    /// Delete on exit.
+    bool deleteOnExit() { return true; }
+
+    /// @see ::Worker::GetTarget().
+    Thread::Target* GetTarget();
+
+    /// The bound thread manager.
+    ThreadMgr* mMgr;
+    /// Our iterator.
+    WorkerList::iterator mItr;
+
+    /// A Mutex to protect this object.
+    Mutex mMutex;
+};
+
+#endif /* !__MT__THREAD_MGR_H__INCL__ */
---
+++ src/common/mt/ThreadMgr.cpp
@@ -0,0 +1,195 @@
+/*
+    ------------------------------------------------------------------------------------
+    LICENSE:
+    ------------------------------------------------------------------------------------
+    This file is part of EVEmu: EVE Online Server Emulator
+    Copyright 2006 - 2008 The EVEmu Team
+    For the latest information visit http://evemu.mmoforge.org
+    ------------------------------------------------------------------------------------
+    This program is free software; you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the Free Software
+    Foundation; either version 2 of the License, or (at your option) any later
+    version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License along with
+    this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+    Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+    http://www.gnu.org/copyleft/lesser.txt.
+    ------------------------------------------------------------------------------------
+    Author:     Bloody.Rabbit
+*/
+
+#include "CommonPCH.h"
+
+#include "log/LogNew.h"
+#include "mt/ThreadMgr.h"
+
+/*************************************************************************/
+/* ThreadMgr                                                             */
+/*************************************************************************/
+ThreadMgr::ThreadMgr( size_t limit )
+: mLimit( limit )
+{
+    sLog.Debug( "ThreadMgr", "Thread manager started" );
+}
+
+ThreadMgr::~ThreadMgr()
+{
+    sLog.Debug( "ThreadMgr", "Thread manager shutting down" );
+
+    SetThreadLimit( 0 );
+}
+
+size_t ThreadMgr::threadCount() const
+{
+    MutexLock lock( mMutex );
+
+    return activeThreadCount() + inactiveThreadCount();
+}
+
+size_t ThreadMgr::activeThreadCount() const
+{
+    MutexLock lock( mMutex );
+
+    return mActiveWorkers.size();
+}
+
+size_t ThreadMgr::inactiveThreadCount() const
+{
+    MutexLock lock( mMutex );
+
+    return mInactiveWorkers.size();
+}
+
+size_t ThreadMgr::threadLimit() const
+{
+    MutexLock lock( mMutex );
+
+    return mLimit;
+}
+
+void ThreadMgr::Run( Thread::Target* target )
+{
+    MutexLock lock( mMutex );
+
+    mTargets.push( target );
+
+    if( 0 < inactiveThreadCount() )
+        // Someone is waiting, signal the condition.
+        mWorkerEvent.Signal();
+    else if( threadCount() < threadLimit() )
+        // Haven't reached the limit yet, spawn a worker.
+        new Worker( this );
+}
+
+void ThreadMgr::SetThreadLimit( size_t limit )
+{
+    MutexLock lock( mMutex );
+
+    mLimit = limit;
+
+    if( threadLimit() < threadCount()
+        && 0 < inactiveThreadCount() )
+    {
+        // Broadcast thread event to make waiting threads quit
+        mWorkerEvent.Broadcast();
+    }
+
+    if( threadLimit() < activeThreadCount() )
+    {
+        // We need to stop some working threads ...
+        for( size_t i = ( activeThreadCount() - threadLimit() ); 0 < i; --i )
+        {
+            Worker* worker = mActiveWorkers.front();
+
+            // This also removes the worker from our list.
+            worker->AssignMgr( NULL );
+        }
+    }
+}
+
+ThreadMgr::WorkerList::iterator ThreadMgr::AddWorker( Worker* worker )
+{
+    MutexLock lock( mMutex );
+
+    mActiveWorkers.push_back( worker );
+    return --mActiveWorkers.end();
+}
+
+void ThreadMgr::RemoveWorker( WorkerList::iterator& itr )
+{
+    MutexLock lock( mMutex );
+
+    mActiveWorkers.erase( itr );
+}
+
+Thread::Target* ThreadMgr::GetTarget( WorkerList::iterator& itr )
+{
+    MutexLock lock( mMutex );
+
+    // move the Worker from mActiveWorkers to mInactiveWorkers
+    mInactiveWorkers.splice( mInactiveWorkers.end(),
+                             mActiveWorkers, itr );
+    itr = --mInactiveWorkers.end();
+
+    Thread::Target* target = NULL;
+    while( threadCount() <= threadLimit() )
+    {
+        if( !mTargets.empty() )
+        {
+            target = mTargets.front();
+            mTargets.pop();
+            break;
+        }
+
+        mWorkerEvent.Wait( mMutex );
+    }
+
+    // move the Worker back
+    mActiveWorkers.splice( mActiveWorkers.end(),
+                           mInactiveWorkers, itr );
+    itr = --mActiveWorkers.end();
+
+    return target;
+}
+
+/*************************************************************************/
+/* ThreadMgr::Worker                                                     */
+/*************************************************************************/
+ThreadMgr::Worker::Worker( ThreadMgr* mgr )
+: mMgr( NULL )
+{
+    AssignMgr( mgr );
+
+    Thread thread( this );
+}
+
+ThreadMgr::Worker::~Worker()
+{
+    AssignMgr( NULL );
+}
+
+void ThreadMgr::Worker::AssignMgr( ThreadMgr* mgr )
+{
+    MutexLock lock( mMutex );
+
+    if( NULL != mMgr )
+        mMgr->RemoveWorker( mItr );
+
+    mMgr = mgr;
+
+    if( NULL != mMgr )
+        mItr = mMgr->AddWorker( this );
+}
+
+Thread::Target* ThreadMgr::Worker::GetTarget()
+{
+    if( NULL == mMgr )
+        return NULL;
+
+    return mMgr->GetTarget( mItr );
+}
