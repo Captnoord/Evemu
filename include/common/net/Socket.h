e5a1d2d2bc11511bf8f3551021de1eedd4b3bded
bloody.rabbit <bloody.rabbit@53638f16-6364-11df-b3cf-17e690f86674>
2010-08-01 13:20:04 +0000

    * rewrote Socket

--- include/common/net/Socket.h
+++ include/common/net/Socket.h
@@ -29,39 +29,197 @@
 #include "net/NetUtils.h"
 
 /**
- * @brief Simple wrapper for sockets.
+ * @brief A network socket.
  *
  * @author Bloody.Rabbit
  */
+template< typename L3 >
 class Socket
 {
 public:
-    Socket( int af, int type, int protocol );
-    ~Socket();
+    /**
+     * @brief The default constructor.
+     */
+    Socket()
+    : mSocket( INVALID_SOCKET ),
+      mSocketAddress( L3::SOCKET_ADDRESS_ANY )
+    {
+    }
+    /**
+     * @brief The primary constructor.
+     *
+     * @param[in] type Type of socket to create.
+     * @param[in] prot Protocol to use.
+     */
+    Socket( int type, int prot )
+    : mSocket( INVALID_SOCKET ),
+      mSocketAddress( L3::SOCKET_ADDRESS_ANY )
+    {
+        int code = Create( type, prot );
+        assert( 0 == code );
+    }
+    /**
+     * @brief A destructor, closes the socket.
+     */
+    ~Socket()
+    {
+        int code = Assign( INVALID_SOCKET, L3::SOCKET_ADDRESS_ANY );
+        assert( 0 == code );
+    }
 
-    int connect( const sockaddr* name, unsigned int namelen );
+    /// Obtains validity of the socket.
+    bool isValid() const { return INVALID_SOCKET != mSocket; }
 
-    unsigned int recv( void* buf, unsigned int len, int flags );
-    unsigned int recvfrom( void* buf, unsigned int len, int flags, sockaddr* from, unsigned int* fromlen );
-    unsigned int send( const void* buf, unsigned int len, int flags );
-    unsigned int sendto( const void* buf, unsigned int len, int flags, const sockaddr* to, unsigned int tolen );
+    /// Obtains the socket address.
+    const typename L3::SocketAddress& socketAddress() const { return mSocketAddress; }
 
-    int bind( const sockaddr* name, unsigned int namelen );
-    int listen( int backlog = SOMAXCONN );
+    /**
+     * @brief Creates a new socket.
+     *
+     * Any old socket is closed before creating a new one.
+     *
+     * @param[in] type Type of socket to create.
+     * @param[in] prot Protocol to use.
+     *
+     * @return An error code.
+     */
+    int Create( int type, int prot )
+    {
+        SOCKET socket = ::socket( L3::ADDRESS_FAMILY, type, prot );
+        if( INVALID_SOCKET == socket )
+            return NET_ERRNO;
 
-    Socket* accept( sockaddr* addr, unsigned int* addrlen );
+        int code = Assign( socket, L3::SOCKET_ADDRESS_ANY );
+        if( 0 != code )
+            return code;
 
-    int setopt( int level, int optname, const void* optval, unsigned int optlen );
+        return 0;
+    }
+    /**
+     * @brief Closes the current socket.
+     *
+     * @return An error code.
+     */
+    int Close()
+    {
+        /* There is a problem that we may be shutting down
+           a listening socket, for which the shutdown will
+           fail. Thus we ignore ENOTCONN error. */
+        if( 0 != ::shutdown( mSocket, SHUT_WR ) )
+            if( ENOTCONN != NET_ERRNO )
+                return NET_ERRNO;
+
+        if( 0 != ::shutdown( mSocket, SHUT_RD ) )
+            if( ENOTCONN != NET_ERRNO )
+                return NET_ERRNO;
+
+        if( 0 != ::close( mSocket ) )
+            return NET_ERRNO;
+
+        mSocket = INVALID_SOCKET;
+        mSocketAddress = L3::SOCKET_ADDRESS_ANY;
+        return 0;
+    }
+
+    /**
+     * @brief Binds the socket to an address.
+     *
+     * @param[in] socketAddress The address to bind to.
+     *
+     * @return An error code.
+     */
+    int Bind( const typename L3::SocketAddress& socketAddress )
+    {
+        if( 0 != ::bind( mSocket,
+                         reinterpret_cast< const sockaddr* >( &socketAddress ),
+                         sizeof( socketAddress ) ) )
+            return NET_ERRNO;
+
+        mSocketAddress = socketAddress;
+        return 0;
+    }
+
+    /**
+     * @brief Sets a socket option.
+     *
+     * @param[in] level Level of option.
+     * @param[in] name  Name of the option.
+     * @param[in] val   Value of the option.
+     *
+     * @return An error code.
+     */
+    template< typename T >
+    int SetOption( int level, int name, const T& val )
+    {
+        if( 0 != ::setsockopt( mSocket, level, name, &val, sizeof( val ) ) )
+            return NET_ERRNO;
+
+        return 0;
+    }
+
 #ifdef WIN32
-    int ioctl( long cmd, unsigned long* argp );
-#else
-    int fcntl( int cmd, long arg );
+    /**
+     * @brief Sets advances options of socket.
+     *
+     * @param[in] cmd A command to perform.
+     * @param[in] arg An argument for command.
+     *
+     * @return An error code.
+     */
+    int Ioctl( long cmd, unsigned long arg )
+    {
+        if( 0 != ::ioctlsocket( mSocket, cmd, &arg ) )
+            return NET_ERRNO;
+
+        return 0;
+    }
+#else /* !WIN32 */
+    /**
+     * @brief Sets advanced options of socket.
+     *
+     * @param[in] cmd The command to perform.
+     * @param[in] arg An argument for command.
+     *
+     * @return An error code.
+     */
+    int Fcntl( int cmd, long arg )
+    {
+        if( 0 != ::fcntl( mSocket, cmd, arg ) )
+            return NET_ERRNO;
+
+        return 0;
+    }
 #endif /* !WIN32 */
 
 protected:
-    Socket( SOCKET sock );
+    /**
+     * @brief Assigns a different socket.
+     *
+     * Any previous socket is closed.
+     *
+     * @param[in] socket        The new socket.
+     * @param[in] socketAddress The address the @a socket is bound to.
+     *
+     * @return An error code.
+     */
+    int Assign( SOCKET socket, const typename L3::SocketAddress& socketAddress )
+    {
+        if( isValid() )
+        {
+            int code = Close();
+            if( 0 != code )
+                return code;
+        }
 
-    SOCKET mSock;
+        mSocket = socket;
+        mSocketAddress = socketAddress;
+        return 0;
+    }
+
+    /// The socket itself.
+    SOCKET mSocket;
+    /// The socket address this socket is associated with.
+    typename L3::SocketAddress mSocketAddress;
 };
 
 #endif /* !__NET__SOCKET_H__INCL__ */
