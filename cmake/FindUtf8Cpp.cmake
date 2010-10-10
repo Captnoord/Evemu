b3f9f46f763ac0f65ffd67043f533235d6c7174c
bloody.rabbit <bloody.rabbit@53638f16-6364-11df-b3cf-17e690f86674>
2010-10-10 10:28:06 +0000

    * redone CMakeLists

--- cmake/FindUtf8Cpp.cmake
+++ cmake/FindUtf8Cpp.cmake
@@ -1,13 +1,13 @@
-# - Find UTF8CPP
-# Find the native UTF8CPP includes
+# - Find Utf8Cpp
+# Find the native Utf8Cpp includes
 #
-#   UTF8CPP_FOUND       - True if UTF8CPP found.
+#   UTF8CPP_FOUND       - True if Utf8Cpp found.
 #   UTF8CPP_INCLUDE_DIR - where to find utf8.h, etc.
 #
 
 IF( UTF8CPP_INCLUDE_DIR )
     # Already in cache, be silent
-    SET( UTF8CPP_FIND_QUIETLY TRUE )
+    SET( Utf8Cpp_FIND_QUIETLY TRUE )
 ENDIF( UTF8CPP_INCLUDE_DIR )
 
 FIND_PATH( UTF8CPP_INCLUDE_DIR "utf8.h"
@@ -16,6 +16,6 @@ FIND_PATH( UTF8CPP_INCLUDE_DIR "utf8.h"
 # handle the QUIETLY and REQUIRED arguments and set UTF8CPP_FOUND to TRUE if
 # all listed variables are TRUE
 INCLUDE( "FindPackageHandleStandardArgs" )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( "UTF8CPP" DEFAULT_MSG UTF8CPP_INCLUDE_DIR )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( "Utf8Cpp" DEFAULT_MSG UTF8CPP_INCLUDE_DIR )
 
 MARK_AS_ADVANCED( UTF8CPP_INCLUDE_DIR )
