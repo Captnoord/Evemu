1d13787f2962eb44eb87cd4670ad7e26756f1ec5
captnoord <captnoord@53638f16-6364-11df-b3cf-17e690f86674>
2010-11-19 21:11:38 +0000

    * finished up on the first stage of the attribute rewrite. Note: it compiles but doesn't work.

--- cmake/FindMySQL.cmake
+++ cmake/FindMySQL.cmake
@@ -3,34 +3,26 @@
 # http://www.cmake.org/Wiki/CMakeUserFindMySQL
 # Slightly modified
 #
-#   MYSQL_FOUND       - True if MySQL found.
-#   MYSQL_INCLUDE_DIR - where to find mysql.h, etc.
-#   MYSQL_LIBRARIES   - List of libraries when using MySQL.
+#  MYSQL_FOUND       - True if MySQL found.
+#  MYSQL_INCLUDE_DIR - where to find mysql.h, etc.
+#  MYSQL_LIBRARIES   - List of libraries when using MySQL.
 #
 
 IF( MYSQL_INCLUDE_DIR )
-    # Already in cache, be silent
-    SET( MySQL_FIND_QUIETLY TRUE )
+  # Already in cache, be silent
+  SET( MySQL_FIND_QUIETLY TRUE )
 ENDIF( MYSQL_INCLUDE_DIR )
 
-FIND_PATH( MYSQL_INCLUDE_DIR "mysql.h"
-           PATH_SUFFIXES "mysql" )
+FIND_PATH( MYSQL_INCLUDE_DIR mysql.h
+           PATH_SUFFIXES mysql )
 
-# we need to find the dynamic library here; it seems to be
-# mysqlclient on Linux, but libmysql on Windows ...
-IF( WIN32 )
-    FIND_LIBRARY( MYSQL_LIBRARIES
-                  NAMES "libmysql"
-                  PATH_SUFFIXES "mysql" )
-ELSE( WIN32 )
-    FIND_LIBRARY( MYSQL_LIBRARIES
-                  NAMES "mysqlclient_r" "mysqlclient"
-                  PATH_SUFFIXES "mysql" )
-ENDIF( WIN32 )
+FIND_LIBRARY( MYSQL_LIBRARIES
+              NAMES mysqlclient mysqlclient_r
+              PATH_SUFFIXES mysql )
 
 # handle the QUIETLY and REQUIRED arguments and set MYSQL_FOUND to TRUE if
 # all listed variables are TRUE
-INCLUDE( "FindPackageHandleStandardArgs" )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( "MySQL" DEFAULT_MSG MYSQL_INCLUDE_DIR MYSQL_LIBRARIES )
+INCLUDE( FindPackageHandleStandardArgs )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( MySQL DEFAULT_MSG MYSQL_INCLUDE_DIR MYSQL_LIBRARIES )
 
 MARK_AS_ADVANCED( MYSQL_INCLUDE_DIR MYSQL_LIBRARIES )
--- cmake/TestX64.cmake
+++ cmake/TestX64.cmake
@@ -1,15 +1,12 @@
 # - Define macro to determine architecture
+# Check if the system is 64bit or not
 #
-# TEST_X64( VARIABLE )
-#     Check if the system is 64bit or not
-#     Arguments:
-#         VARIABLE - variable to store the result to
+#  TEST_X64( VARIABLE )
+#    VARIABLE - variable to store the result to
 #
 
 MACRO( TEST_X64 VARIABLE )
     IF( NOT DEFINED ${VARIABLE} )
-        MESSAGE( STATUS "Check if the system is 64bit" )
-
         IF( CMAKE_SIZEOF_VOID_P EQUAL 8 )
             MESSAGE( STATUS "Check if the system is 64bit - yes" )
             SET( ${VARIABLE} 1 CACHE INTERNAL "Result of TEST_X64" FORCE )
