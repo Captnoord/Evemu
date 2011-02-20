a59988f83197460736dd841118ef358f8d49fb52
bloody.rabbit <bloody.rabbit@53638f16-6364-11df-b3cf-17e690f86674>
2011-02-20 13:42:43 +0000

    * updated license in eve-xmlpktgen

--- include/eve/eve-xmlpktgen/EncodeGenerator.h
+++ include/eve/eve-xmlpktgen/EncodeGenerator.h
@@ -1,30 +1,30 @@
-/*
-	------------------------------------------------------------------------------------
-	LICENSE:
-	------------------------------------------------------------------------------------
-	This file is part of EVEmu: EVE Online Server Emulator
-	Copyright 2006 - 2008 The EVEmu Team
-	For the latest information visit http://evemu.mmoforge.org
-	------------------------------------------------------------------------------------
-	This program is free software; you can redistribute it and/or modify it under
-	the terms of the GNU Lesser General Public License as published by the Free Software
-	Foundation; either version 2 of the License, or (at your option) any later
-	version.
+/**
+ * @file EncodeGenerator.h
+ *
+ * This file is part of EVEmu: EVE Online Server Emulator.<br>
+ * Copyright (C) 2006-2011 The EVEmu Team<br>
+ * For the latest information visit <i>http://evemu.org</i>.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+ * <i>http://www.gnu.org/copyleft/lesser.txt</i>.
+ *
+ * @author Zhur
+ */
 
-	This program is distributed in the hope that it will be useful, but WITHOUT
-	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-	FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-
-	You should have received a copy of the GNU Lesser General Public License along with
-	this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-	Place - Suite 330, Boston, MA 02111-1307, USA, or go to
-	http://www.gnu.org/copyleft/lesser.txt.
-	------------------------------------------------------------------------------------
-	Author:		Zhur
-*/
-
-#ifndef __ENCODEGENERATOR_H_INCL__
-#define __ENCODEGENERATOR_H_INCL__
+#ifndef __ENCODE_GENERATOR_H__INCL__
+#define __ENCODE_GENERATOR_H__INCL__
 
 #include "Generator.h"
 
@@ -32,13 +32,13 @@ class ClassEncodeGenerator
 : public Generator
 {
 public:
-	ClassEncodeGenerator( FILE* outputFile = NULL );
+    ClassEncodeGenerator( FILE* outputFile = NULL );
 
 protected:
-	const char* top() const { return mVariableStack.top().c_str(); }
-	void pop() { mVariableStack.pop(); }
-	void push(const char *v) { mVariableStack.push( v ); }
-	void clear() { while( !mVariableStack.empty() ) pop(); }
+    const char* top() const { return mVariableStack.top().c_str(); }
+    void pop() { mVariableStack.pop(); }
+    void push(const char *v) { mVariableStack.push( v ); }
+    void clear() { while( !mVariableStack.empty() ) pop(); }
 
     bool ProcessElementDef( const TiXmlElement* field );
     bool ProcessElement( const TiXmlElement* field );
@@ -80,11 +80,9 @@ protected:
     bool ProcessSubStructInline( const TiXmlElement* field );
 
 private:
-	uint32 mItemNumber;
-	std::stack<std::string> mVariableStack;
-	const char* mName;
+    uint32 mItemNumber;
+    std::stack<std::string> mVariableStack;
+    const char* mName;
 };
 
-#endif
-
-
+#endif /* !__ENCODE_GENERATOR_H__INCL__ */
--- include/eve/eve-xmlpktgen/HeaderGenerator.h
+++ include/eve/eve-xmlpktgen/HeaderGenerator.h
@@ -1,30 +1,30 @@
-/*
-	------------------------------------------------------------------------------------
-	LICENSE:
-	------------------------------------------------------------------------------------
-	This file is part of EVEmu: EVE Online Server Emulator
-	Copyright 2006 - 2008 The EVEmu Team
-	For the latest information visit http://evemu.mmoforge.org
-	------------------------------------------------------------------------------------
-	This program is free software; you can redistribute it and/or modify it under
-	the terms of the GNU Lesser General Public License as published by the Free Software
-	Foundation; either version 2 of the License, or (at your option) any later
-	version.
+/**
+ * @file HeaderGenerator.h
+ *
+ * This file is part of EVEmu: EVE Online Server Emulator.<br>
+ * Copyright (C) 2006-2011 The EVEmu Team<br>
+ * For the latest information visit <i>http://evemu.org</i>.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+ * <i>http://www.gnu.org/copyleft/lesser.txt</i>.
+ *
+ * @author Zhur
+ */
 
-	This program is distributed in the hope that it will be useful, but WITHOUT
-	ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-	FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-
-	You should have received a copy of the GNU Lesser General Public License along with
-	this program; if not, write to the Free Software Foundation, Inc., 59 Temple
-	Place - Suite 330, Boston, MA 02111-1307, USA, or go to
-	http://www.gnu.org/copyleft/lesser.txt.
-	------------------------------------------------------------------------------------
-	Author:		Zhur
-*/
-
-#ifndef __HEADERGENERATOR_H_INCL__
-#define __HEADERGENERATOR_H_INCL__
+#ifndef __HEADER_GENERATOR_H__INCL__
+#define __HEADER_GENERATOR_H__INCL__
 
 #include "Generator.h"
 
@@ -32,11 +32,11 @@ class ClassHeaderGenerator
 : public Generator
 {
 public:
-	ClassHeaderGenerator( FILE* outputFile = NULL );
+    ClassHeaderGenerator( FILE* outputFile = NULL );
 
 protected:
-	bool RegisterName( const char* name, uint32 row );
-	void ClearNames();
+    bool RegisterName( const char* name, uint32 row );
+    void ClearNames();
 
     void RegisterProcessors();
 
@@ -81,9 +81,7 @@ protected:
     bool ProcessSubStructInline( const TiXmlElement* field );
 
 private:
-	std::set<std::string> mNamesUsed;
+    std::set<std::string> mNamesUsed;
 };
 
-#endif
-
-
+#endif /* !__HEADER_GENERATOR_H__INCL__ */
